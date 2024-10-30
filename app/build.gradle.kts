658ba97eedc45afe4ffbd45834a6a676bdebf707
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2024-10-30 21:54:12 +0100

    Configured hilt in Application class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.hilt.android.gradle.plugin)
 
     id("kotlin-kapt")
