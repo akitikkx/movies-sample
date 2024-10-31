26d8651705694e523aec1f530d986682bcd6f19b
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2024-10-31 19:59:11 +0100

    Encapsulated the return type of the getBoxOfficeMovies inside kotlin.Result

--- app/src/main/java/com/example/moviessample/data/TraktApiService.kt
+++ app/src/main/java/com/example/moviessample/data/TraktApiService.kt
@@ -1,11 +1,10 @@
 package com.example.moviessample.data
 
 import com.example.moviessample.data.models.trakt.BoxOfficeResponse
-import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 
 interface TraktApiService {
 
     @GET("movies/boxoffice")
-    suspend fun getBoxOfficeMovies(): Deferred<BoxOfficeResponse>
+    suspend fun getBoxOfficeMovies(): Result<BoxOfficeResponse>
 }
