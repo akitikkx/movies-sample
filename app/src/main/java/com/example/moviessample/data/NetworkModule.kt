178f19627f297103abd58deaae1d60a66eebed60
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2024-10-31 20:00:22 +0100

    Defined separate clients for tmdb and trakt inside NetworkModule due to difference in call params

--- app/src/main/java/com/example/moviessample/data/NetworkModule.kt
+++ app/src/main/java/com/example/moviessample/data/NetworkModule.kt
@@ -1,6 +1,7 @@
 package com.example.moviessample.data
 
 import android.content.Context
+import com.example.moviessample.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -8,7 +9,10 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.Cache
 import okhttp3.ConnectionPool
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -25,7 +29,7 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideOkhttpClient(@ApplicationContext context: Context) : OkHttpClient {
+    fun provideTraktClient(@ApplicationContext context: Context) : OkHttpClient {
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
 
@@ -40,10 +44,37 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideTmdbApiService(okHttpClient: OkHttpClient): TmdbApiService {
+    fun provideTmdbNetworkClient(@ApplicationContext context: Context) : OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(
+                object : Interceptor {
+                    override fun intercept(chain: Interceptor.Chain): Response {
+                        val request: Request = chain.request()
+                            .newBuilder()
+                            .header("accept", "application/json")
+                            .header("Authorization", BuildConfig.TMDB_ACCESS_TOKEN)
+                            .build()
+                        return chain.proceed(request)
+                    }
+                }
+            )
+            .cache(Cache(context.cacheDir, (5 * 1024 * 1024).toLong()))
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectionPool(ConnectionPool(0, 1, TimeUnit.SECONDS))
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTmdbApiService(tmdbNetworkClient: OkHttpClient): TmdbApiService {
         return Retrofit.Builder()
             .baseUrl(TMDB_BASE_URL)
-            .client(okHttpClient)
+            .client(tmdbNetworkClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(TmdbApiService::class.java)
@@ -51,10 +82,10 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideTraktApiService(okHttpClient: OkHttpClient): TraktApiService {
+    fun provideTraktApiService(traktNetworkClient: OkHttpClient): TraktApiService {
         return Retrofit.Builder()
             .baseUrl(TRAKT_BASE_URL)
-            .client(okHttpClient)
+            .client(traktNetworkClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(TraktApiService::class.java)
