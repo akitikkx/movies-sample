9389bcc4cc4fad14ee5cb42ccee374f2d62e4fa9
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2024-11-01 14:00:44 +0100

    Updated TmdbRepository getMovieDetails to return a Flow<Result<MovieDetails>>

--- app/src/main/java/com/example/moviessample/data/TmdbRepository.kt
+++ app/src/main/java/com/example/moviessample/data/TmdbRepository.kt
@@ -1,8 +1,10 @@
 package com.example.moviessample.data
 
+import com.example.moviessample.data.util.Result
 import com.example.moviessample.domain.MovieDetails
+import kotlinx.coroutines.flow.Flow
 
 interface TmdbRepository {
 
-    suspend fun getMovieDetails(movieId: Int): Result<MovieDetails>
+    suspend fun getMovieDetails(movieId: Int): Flow<Result<MovieDetails>>
 }
--- app/src/main/java/com/example/moviessample/data/TmdbRepositoryImpl.kt
+++ app/src/main/java/com/example/moviessample/data/TmdbRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.example.moviessample.data
 
-import com.example.moviessample.data.models.tmdb.MovieDetailsResponse
+import com.example.moviessample.data.util.Result
 import com.example.moviessample.domain.MovieDetails
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -10,25 +13,22 @@ class TmdbRepositoryImpl @Inject constructor(
     private val tmdbApiService: TmdbApiService
 ) : TmdbRepository {
 
-    override suspend fun getMovieDetails(movieId: Int): Result<MovieDetails> {
-        return try {
-            val result = withContext(Dispatchers.IO) {
-                tmdbApiService.getDetails(
-                    movieId = movieId,
-                    apiKey = ""
+    override suspend fun getMovieDetails(movieId: Int): Flow<Result<MovieDetails>> = flow {
+        emit(Result.Loading)
+        val result = withContext(Dispatchers.IO) {
+            try {
+                val details = tmdbApiService.getDetails(movieId)
+                val movieDetails = MovieDetails(
+                    id = details.id,
+                    posterPath = details.poster_path,
+                    overview = details.overview,
+                    tagline = details.tagline
                 )
+                Result.Success(movieDetails)
+            } catch (e: Exception) {
+                Result.Failure(e)
             }
-            result.map { response ->
-                MovieDetails(
-                    id = response.id,
-                    posterPath = response.poster_path,
-                    overview =  response.overview,
-                    tagline = response.tagline
-                )
-            }
-
-        } catch (e: Exception) {
-            Result.failure(e)
         }
-    }
+        emit(result)
+    }.flowOn(Dispatchers.IO)
 }
