1b589d85b7f80517a2443ecfdd6b8cd5e939a7c9
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2024-11-01 14:01:57 +0100

    Updated TraktRepository getMovieDetails to return a Flow<Result<List<BoxOfficeMovie>>>

--- app/src/main/java/com/example/moviessample/data/TraktRepository.kt
+++ app/src/main/java/com/example/moviessample/data/TraktRepository.kt
@@ -1,8 +1,10 @@
 package com.example.moviessample.data
 
+import com.example.moviessample.data.util.Result
 import com.example.moviessample.domain.BoxOfficeMovie
+import kotlinx.coroutines.flow.Flow
 
 interface TraktRepository {
 
-    suspend fun getBoxOfficeMovies(): Result<List<BoxOfficeMovie>>
+    suspend fun getBoxOfficeMovies(): Flow<Result<List<BoxOfficeMovie>>>
 }
--- app/src/main/java/com/example/moviessample/data/TraktRepositoryImpl.kt
+++ app/src/main/java/com/example/moviessample/data/TraktRepositoryImpl.kt
@@ -1,7 +1,11 @@
 package com.example.moviessample.data
 
+import com.example.moviessample.data.util.Result
 import com.example.moviessample.domain.BoxOfficeMovie
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -9,20 +13,27 @@ class TraktRepositoryImpl @Inject constructor(
     private val traktApiService: TraktApiService
 ) : TraktRepository {
 
-    override suspend fun getBoxOfficeMovies(): Result<List<BoxOfficeMovie>> {
-        return try {
-            val result = withContext(Dispatchers.IO) { traktApiService.getBoxOfficeMovies() }
-
-            result.map { boxOfficeResponse ->
-                boxOfficeResponse.map { item ->
-                    BoxOfficeMovie(
-                        title = item.movie.title,
-                        year = item.movie.year
-                    )
-                }
+    override suspend fun getBoxOfficeMovies(): Flow<Result<List<BoxOfficeMovie>>> = flow {
+        emit(Result.Loading)
+        val result = withContext(Dispatchers.IO) {
+            try {
+                val movies = traktApiService.getBoxOfficeMovies()
+                    .map { item ->
+                        BoxOfficeMovie(
+                            title = item.movie.title,
+                            year = item.movie.year,
+                            imdb = item.movie.ids.imdb,
+                            slug = item.movie.ids.slug,
+                            tmdb = item.movie.ids.tmdb,
+                            trakt = item.movie.ids.trakt
+                        )
+                    }
+                Result.Success(movies) // Wrap the result in Success
+            } catch (e: Exception) {
+                Result.Failure(e) // Wrap the exception in Failure
             }
-        } catch (e: Exception) {
-            Result.failure(e)
         }
-    }
+        emit(result)
+    }.flowOn(Dispatchers.IO)
+
 }
