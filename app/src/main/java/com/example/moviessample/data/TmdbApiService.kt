3494d66ad48a8d94d3b6521c93f7b06c3239d222
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2024-11-01 13:56:30 +0100

    Removed Result encapsulation in TmdbApiService

--- app/src/main/java/com/example/moviessample/data/TmdbApiService.kt
+++ app/src/main/java/com/example/moviessample/data/TmdbApiService.kt
@@ -3,13 +3,11 @@ package com.example.moviessample.data
 import com.example.moviessample.data.models.tmdb.MovieDetailsResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface TmdbApiService {
 
     @GET("movie/{movieId}")
     suspend fun getDetails(
         @Path("movieId") movieId: Int,
-        @Query("api_key") apiKey: String
-    ): Result<MovieDetailsResponse>
+    ): MovieDetailsResponse
 }
