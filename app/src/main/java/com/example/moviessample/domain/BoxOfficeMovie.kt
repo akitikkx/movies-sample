d571978267e7547bbd8ccce9814ff0e0962636ca
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2024-11-01 13:59:20 +0100

    Added more attributes to BoxOfficeMovie

--- app/src/main/java/com/example/moviessample/domain/BoxOfficeMovie.kt
+++ app/src/main/java/com/example/moviessample/domain/BoxOfficeMovie.kt
@@ -1,7 +1,14 @@
 package com.example.moviessample.domain
 
 data class BoxOfficeMovie(
-    val id: Int? = null,
     val title: String,
     val year: Int,
+    val id: Int? = null,
+    val overview: String? = null,
+    val posterPath: String? = null,
+    val tagline: String? = null,
+    val imdb: String? = null,
+    val slug: String? = null,
+    val tmdb: Int? = null,
+    val trakt: Int? = null,
 )
