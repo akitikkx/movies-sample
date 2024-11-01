17ddd588f4673be398ddb9d36d2e33d847e07492
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2024-11-01 22:11:44 +0100

    Added coil-compose; updated dependencies and compileSdk version to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,12 +14,12 @@ localProperties.load(FileInputStream(rootProject.file("local.properties")))
 
 android {
     namespace = "com.example.moviessample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.moviessample"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -90,6 +90,9 @@ dependencies {
     // OkHttp
     implementation(libs.okhttp3)
     implementation(libs.okhttp3.logging.interceptor)
+
+    // Coil
+    implementation(libs.coil)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
